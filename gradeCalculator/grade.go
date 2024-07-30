fa6eb86b41ed2316205b35a5d9df025b61a0ea0d
eyu-ted <eyuted81619@gmail.com>
2024-07-31 00:22:49 +0300

    frequency and isPalindrome

--- gradeCalculator/grade.go
+++ gradeCalculator/grade.go
@@ -1,13 +1,10 @@
-
-
 package main
 
 import (
 	"fmt"
 )
 
-// CalculateAverageGrade calculates the average of the given grades.
-func CalculateAverageGrade(grades []float64) float64 {
+func CalculateAverageGrade(grades map[string]float64) float64 {
 	sum := 0.0
 	for _, grade := range grades {
 		sum += grade
@@ -15,8 +12,7 @@ func CalculateAverageGrade(grades []float64) float64 {
 	return sum / float64(len(grades))
 }
 
-// AcceptInput accepts input from the user and returns the student's name, subjects, and grades.
-func AcceptInput() (string, []string, []float64) {
+func AcceptInput() (string, map[string]float64) {
 	var studentName string
 	var numSubjects int
 
@@ -30,38 +26,38 @@ func AcceptInput() (string, []string, []float64) {
 		_, err = fmt.Scanln(&numSubjects)
 	}
 
-	subjects := make([]string, numSubjects)
-	grades := make([]float64, numSubjects)
+	record := make(map[string]float64)
 
 	for i := 0; i < numSubjects; i++ {
-		fmt.Printf("Enter the name of subject %d: ", i+1)
-		fmt.Scan(&subjects[i])
+		var subject string
+		var grade float64
 
-		fmt.Printf("Enter the grade for %s: ", subjects[i])
-		_, err := fmt.Scan(&grades[i])
-		for err != nil || grades[i] < 0 || grades[i] > 100 {
+		fmt.Printf("Enter the name of subject %d: ", i+1)
+		fmt.Scanln(&subject)
+
+		fmt.Printf("Enter the grade for %s: ", subject)
+		_, err := fmt.Scanln(&grade)
+		for err != nil || grade < 0 || grade > 100 {
 			fmt.Println("Invalid grade. Please enter a number between 0 and 100.")
-			_, err = fmt.Scan(&grades[i])
+			_, err = fmt.Scanln(&grade)
 		}
+		record[subject] = grade
 	}
 
-	return studentName, subjects, grades
+	return studentName, record
 }
 
-// DisplayResult displays the student's name, subjects, grades, and average grade.
-func DisplayResult(studentName string, subjects []string, grades []float64, averageGrade float64) {
+func DisplayResult(studentName string, record map[string]float64, averageGrade float64) {
 	fmt.Printf("\nStudent Name: %s\n", studentName)
 	fmt.Println("Subject Grades:")
-	for i, subject := range subjects {
-		fmt.Printf("%s: %.2f\n", subject, grades[i])
+	for subject, grade := range record {
+		fmt.Printf("%s: %.2f\n", subject, grade)
 	}
 	fmt.Printf("Average Grade: %.2f\n", averageGrade)
 }
 
-// main is the entry point of the program.
 func main() {
-	studentName, subjects, grades := AcceptInput()
-	averageGrade := CalculateAverageGrade(grades)
-	DisplayResult(studentName, subjects, grades, averageGrade)
+	studentName, record := AcceptInput()
+	averageGrade := CalculateAverageGrade(record)
+	DisplayResult(studentName, record, averageGrade)
 }
-
